922958fcf26179c2ac322f39af9c196f7e42552c
Rahulyadav199799 <100790896+Rahulyadav199799@users.noreply.github.com>
2023-04-11 15:18:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_subnet" "PublicSubnet" {
   
 resource "aws_subnet" "PrivateSubnet" {
   vpc_id     = aws_vpc.Rahulvpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.2.0/24"
 
   tags = {
     Name = "PrivateSN"
