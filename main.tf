3d4349afda7494282cf78ec23d32d686a741ed31
Rahulyadav199799 <100790896+Rahulyadav199799@users.noreply.github.com>
2023-04-11 14:43:00 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,45 @@
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_vpc" "Rahulvpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "RahulVPC"
+  }
+}
+
+resource "aws_subnet" "PublicSubnet" {
+  vpc_id     = aws_vpc.Rahulvpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = True
+
+  tags = {
+    Name = "PublicSN"
+  }
+}
+  
+resource "aws_subnet" "PrivateSubnet" {
+  vpc_id     = aws_vpc.Rahulvpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "PrivateSN"
+  }
+}
+resource "aws_internet_gateway" "myigw" {
+  vpc_id = aws_vpc.Rahulvpc.id
+
+  tags = {
+    Name = "MyIgw"
+  }
+}
+
+route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.myigw.id
+}
